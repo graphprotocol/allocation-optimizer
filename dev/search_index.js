14d2e7683d96e2608f880f7e79244b2700f96b3c
Documenter.jl <documenter@juliadocs.github.io>
2022-03-29 17:04:50 +0000

    build based on 1f09458

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AllocationOpt","category":"page"},{"location":"#AllocationOpt","page":"Home","title":"AllocationOpt","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AllocationOpt.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AllocationOpt]","category":"page"}]
+}
