57eb48463a5c0c2375af0acbeba7ad39592b7198
Anirudh Patel <patel.anirudh0025@gmail.com>
2022-05-19 11:30:52 -0700

    feat: Standardise optimiser inputs

--- src/AllocationOpt.jl
+++ src/AllocationOpt.jl
@@ -8,7 +8,7 @@ include("query.jl")
 include("service.jl")
 
 """
-    function optimize_indexer(id, whitelist, blacklist, pinnedlist, frozenlist)
+    function optimize_indexer(id, whitelist, blacklist, pinnedlist, frozenlist, grtgas, minimum_allocation_amount)
 
 # Arguments
 - `id::AbstractString`: The id of the indexer to optimise.
@@ -16,19 +16,20 @@ include("service.jl")
 - `blacklist::Vector{AbstractString}`: Subgraph deployment IPFS hashes included in this list will not be considered, and will be suggested to close if there's an existing allocation.
 - `pinnedlist::Vector{AbstractString}`: Subgraph deployment IPFS hashes included in this list will be guaranteed allocation. Currently unsupported.
 - `frozenlist::Vector{AbstractString}`: Subgraph deployment IPFS hashes included in this list will not be considered during optimisation. Any allocations you have on these subgraphs deployments will remain.
+- `grtgas::Real`: The maximum amount of GRT that you are willing to spend on each allocation transaction.
+- `minimum_allocation_amount::Real`: The minimum amount of GRT that you are willing to allocate to a subgraph.
+- `allocation_lifetime::Integer`: The number of epoches you assume to open each allocations for. Lifetime 1 means you are reallocating every epoch. 
     
 # Examples
 ```julia-repl
 julia> using AllocationOpt
-julia> optimize_indexer("0x6ac85b9d834b51b14a7b0ed849bb5199e04c05c5", String["QmP4oSiQ7Wc4JTFk86m2JxGvR912NyBbxJnEdZawkYLTk4"], String[], String[], String[])
+julia> optimize_indexer("0x6ac85b9d834b51b14a7b0ed849bb5199e04c05c5", String["QmP4oSiQ7Wc4JTFk86m2JxGvR912NyBbxJnEdZawkYLTk4"], String[], String[], String[], 0.0, 0.0, 1)
 1-element Vector{Tuple{String, Float64}}:
  ("QmP4oSiQ7Wc4JTFk86m2JxGvR912NyBbxJnEdZawkYLTk4", 5.539482411224138e6)
-julia> optimize_indexer("foo", String["QmP4oSiQ7Wc4JTFk86m2JxGvR912NyBbxJnEdZawkYLTk4"], String[], String[], String[])
+julia> optimize_indexer("foo", String["QmP4oSiQ7Wc4JTFk86m2JxGvR912NyBbxJnEdZawkYLTk4"], String[], String[], String[], 0.0, 0.0, 1)
 ERROR: AllocationOpt.UnknownIndexerError()
-julia> optimize_indexer("0x6ac85b9d834b51b14a7b0ed849bb5199e04c05c5", String["foo"], String[], String[], String[])
+julia> optimize_indexer("0x6ac85b9d834b51b14a7b0ed849bb5199e04c05c5", String["foo"], String[], String[], String[], 0.0, 0.0, 1)
 ERROR: AllocationOpt.BadSubgraphIpfsHashError()
-Stacktrace:
- [1] optimize_indexer(id::String, whitelist::Vector{String}, blacklist::Vector{String}, pinnedlist::Vector{String}, frozenlist::Vector{String})
 ```
 """
 function optimize_indexer(
@@ -37,12 +38,18 @@ function optimize_indexer(
     blacklist::Vector{T},
     pinnedlist::Vector{T},
     frozenlist::Vector{T},
+    grtgas::Real,
+    minimum_allocation_amount::Real,
+    allocation_lifetime::Integer,
 ) where {T<:AbstractString}
     userlists = vcat(whitelist, blacklist, pinnedlist, frozenlist)
     if !verify_ipfshashes(userlists)
         throw(BadSubgraphIpfsHashError())
     end
 
+    @warn "grtgas is not currently optimised for."
+    @warn "minimum_allocation_amount is not currently optimised for."
+    @warn "allocation_lifetime is not currently optimised for."
     if !isempty(pinnedlist)
         @warn "pinnedlist is not currently optimised for."
     end
--- test/runtests.jl
+++ test/runtests.jl
@@ -24,10 +24,17 @@ using Test
         ipfshash = all_hashes[1]
         another_ipfshash = all_hashes[2]
         @test_throws AllocationOpt.UnknownIndexerError optimize_indexer(
-            "0x6ac85b9d834b51b14a7b0ed849bb5199e", String[], String[], String[], String[]
+            "0x6ac85b9d834b51b14a7b0ed849bb5199e",
+            String[],
+            String[],
+            String[],
+            String[],
+            0.0,
+            0.0,
+            1,
         )
         @test_throws AllocationOpt.BadSubgraphIpfsHashError optimize_indexer(
-            id, String[""], String[ipfshash], String[], String[]
+            id, String[""], String[ipfshash], String[], String[], 0.0, 0.0, 1
         )
 
         # Indexer stake should be equal to the sum of allocations
@@ -35,7 +42,9 @@ using Test
         indexer = query(client, "indexers"; query_args=Dict("where" => Dict("id" => id)), output_fields=["delegatedTokens", "stakedTokens"]).data["indexers"][1]
         stake = togrt(indexer["delegatedTokens"]) + togrt(indexer["stakedTokens"])
         # run optimize_indexer
-        allocs = optimize_indexer(id, String[ipfshash], String[], String[], String[])
+        allocs = optimize_indexer(
+            id, String[ipfshash], String[], String[], String[], 0.0, 0.0, 1
+        )
         # Sum allocation amounts
         ω = sum(map(x -> x[2], allocs))
         @test isapprox(ω, stake; atol=1e-6)
@@ -43,7 +52,14 @@ using Test
         @test length(allocs) == 1
         # run optimize_indexer
         allocs = optimize_indexer(
-            id, String[ipfshash, another_ipfshash], String[], String[], String[]
+            id,
+            String[ipfshash, another_ipfshash],
+            String[],
+            String[],
+            String[],
+            0.0,
+            0.0,
+            1,
         )
         # Sum allocation amounts
         ω = sum(map(x -> x[2], allocs))
