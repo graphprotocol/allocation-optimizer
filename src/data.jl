385c2adcf2b7a6936c11d53dda63abe4aeedc579
Anirudh <anirudh@semiotic.ai>
2023-03-08 16:51:12 -0800

    docs: Fix iquery doc
    Signed-off-by: Anirudh <anirudh@semiotic.ai>

--- src/data.jl
+++ src/data.jl
@@ -30,9 +30,6 @@ Return the components of a GraphQL query for indexers.
 
 For use with the TheGraphData.jl package.
 
-!!! note
-    This currently only gets the first 1000 indexers with staked tokens ≥ 100k GRT
-
 ```julia
 julia> using AllocationOpt
 julia> value, args, fields = AllocationOpt.iquery()
