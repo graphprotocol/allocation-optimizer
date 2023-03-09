774159e6c6e98e13271ef76b9916669e42fe2703
Anirudh <anirudh@semiotic.ai>
2023-03-09 11:23:54 -0800

    docs: Example for togrt function
    Signed-off-by: Anirudh <anirudh@semiotic.ai>

--- src/domain.jl
+++ src/domain.jl
@@ -10,5 +10,20 @@ Convert `x` to GRT.
 
 !!! note
     This function is meant to be used with freshly queried data, so it operates on strings.
+
+```julia
+julia> using AllocationOpt
+julia> AllocationOpt.togrt("1")
+```
 """
 togrt(x::AbstractString) = parse(Float64, x) / ethtogrt
+
+"""
+    ipfshash(::Val{:allocation}, x)
+
+Get the ipfs hash of `x` when `x` is part of the allocation table.
+
+
+"""
+ipfshash(::Val{:allocation}, x) = getproperty(x, "subgraphDeployment.ipfsHash")
+ipfshash(::Val{:allocation}, x) = getproperty(x, "subgraphDeployment.ipfsHash")
