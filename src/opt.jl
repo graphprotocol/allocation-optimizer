3bc0c38150ec148f4132ec6926a39018ef2be825
Anirudh <anirudh@semiotic.ai>
2023-03-20 15:37:50 -0700

    docs: opt docs updated with example outputs
    Signed-off-by: Anirudh <anirudh@semiotic.ai>

--- src/opt.jl
+++ src/opt.jl
@@ -12,6 +12,9 @@ julia> Ω = [1.0, 7.0]
 julia> ψ = [10.0, 5.0]
 julia> σ = 5.0
 julia> AllocationOpt.optimizeanalytic(Ω, ψ, σ)
+2-element Vector{Float64}:
+ 3.5283092056122474
+ 1.4716907943877526
 ```
 """
 function optimizeanalytic(Ω, ψ, σ)
@@ -29,14 +32,6 @@ allocations `Ω`, and a dual solution vector `v`.
 
 !!! note
     You should probably not use this function directly. Use [`optimizeanalytic`](@ref) instead.
-
-```julia
-julia> using AllocationOpt
-julia> Ω = [1.0, 7.0]
-julia> ψ = [10.0, 5.0]
-julia> v = [1.0, 10.0, 2.0]
-julia> AllocationOpt.dual(Ω, ψ, v)
-```
 """
 primal(Ω, ψ, v) = max.(0.0, .√(ψ .* Ω / v) - Ω)
 
@@ -48,14 +43,6 @@ allocation vector `Ω`, and stake `σ`.
 
 !!! note
     You should probably not use this function directly. Use [`optimizeanalytic`](@ref) instead.
-
-```julia
-julia> using AllocationOpt
-julia> Ω = [1.0, 7.0]
-julia> ψ = [10.0, 5.0]
-julia> σ = 5.0
-julia> AllocationOpt.dual(Ω, ψ, σ)
-```
 """
 function dual(Ω, ψ, σ)
     lower_bound = eps(Float64)
@@ -84,6 +71,7 @@ julia> Φ = 1.0
 julia> Ψ = 20.0
 julia> g = 0.01
 julia> xs, nonzeros, profits = AllocationOpt.optimize(Ω, ψ, σ, K, Φ, Ψ, g)
+([5.0 2.5; 0.0 2.5], Int32[1, 2], [0.4066666666666667 0.34714285714285714; 0.0 0.34714285714285714])
 ```
 """
 function optimize(Ω, ψ, σ, K, Φ, Ψ, g)
@@ -124,6 +112,9 @@ julia> k = 1
 julia> Φ = 1.0
 julia> Ψ = 20.0
 julia> AllocationOpt.optimizek(xopt, Ω, ψ, σ, k, Φ, Ψ)
+2-element Vector{Float64}:
+ 5.0
+ 0.0
 ```
 """
 function optimizek(xopt, Ω, ψ, σ, k, Φ, Ψ)
@@ -153,6 +144,7 @@ julia> using AllocationOpt
 julia> ψ = [0.0, 1.0]
 julia> Ω = [1.0, 1.0]
 julia> AllocationOpt.lipschitzconstant(ψ, Ω)
+2.0
 ```
 """
 lipschitzconstant(ψ, Ω) = maximum(2 * ψ ./ Ω .^ 3)
@@ -166,6 +158,8 @@ Get the non-zero elements of vector `v`.
 julia> using AllocationOpt
 julia> v = [0.0, 1.0]
 julia> AllocationOpt.nonzero(v)
+1-element view(::Vector{Float64}, [2]) with eltype Float64:
+ 1.0
 ```
 """
 nonzero(v::AbstractVector) = SAC.filterview(x -> x != 0, v)
